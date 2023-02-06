ea5f304b48968c9c48b6e077cade98d168831f34
Mairajk <mairajkhan597@gmail.com>
2023-02-06 11:55:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var a = prompt("enter your city")
+var b = "karachi"
+if(a === b) {
+   alert("“Welcome to city of lights”")
+}
+var a = prompt("enter your Gender")
+var b = "male"
+var c = "female"
+if(a === b) {
+   alert("“Good Morning Sir.”")
+}
+if(a === c) {
+   alert("“Good Morning madam.”")
+}
+var a = prompt("lets learn the signals")
+var b = "red"
+var c = "yellow"
+var d = "green"
+if(a === b) {
+   alert("“Must stop!”")
+}
+if(a === c) {
+   alert("“Ready to move”")
+}
+if(a === d) {
+   alert("“Move now”")
+}
+var a = prompt("first go! and see your remaining fuel")
+var b = "0.25 L"
+if(a === b) {
+   alert("“Please refill the fuel in your car”")
+}
+
+var a = 4;
+var b = prompt("check the wheather")
+var c = 5
+if (a === c){
+alert("true");
+}
+if (a !== c){
+alert("false");
+}
+var a = 82;
+var b = prompt("check the wheather")
+var c = 83
+if (a === c){
+alert("true");
+}
+var a = prompt("check your percentages")
+var b = 80
+var c = 70
+var d = 60
+if (a >= b){
+    alert("A-one.“Exellant!”");
+    }
